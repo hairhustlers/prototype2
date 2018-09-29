3c3dbeea9e0c2d1052e0ff1de364e47aa02871b6
hairhustlers <43693233+hairhustlers@users.noreply.github.com>
2018-09-28 22:42:10 -0700

    Add files via upload

---
+++ Prototype2/Prototype/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Prototype
+//
+//  Created by KAREN NELSON on 8/20/18.
+//  Copyright © 2018 Hair Hustlers. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
